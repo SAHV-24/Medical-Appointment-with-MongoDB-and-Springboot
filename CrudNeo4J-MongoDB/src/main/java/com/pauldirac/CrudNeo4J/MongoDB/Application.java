4e999f26492f767067045864fc21ea8b9e70e786
sergio <sergioandresherreravallejo@gmail.com>
2024-10-30 14:47:05 -0500

    Spring initialization

---
+++ CrudNeo4J-MongoDB/src/main/java/com/pauldirac/CrudNeo4J/MongoDB/Application.java
@@ -0,0 +1,13 @@
+package com.pauldirac.CrudNeo4J.MongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ CrudNeo4J-MongoDB/src/test/java/com/pauldirac/CrudNeo4J/MongoDB/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pauldirac.CrudNeo4J.MongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
