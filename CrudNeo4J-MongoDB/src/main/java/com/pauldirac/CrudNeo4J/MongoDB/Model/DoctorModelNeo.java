caac2d2e44496fb57b452034d1c9343e760c3c48
Jean Paul Delgado <jean.delgado@uao.edu.co>
2024-11-19 18:21:49 -0500

    added 'DoctorModelNeo' for medics stored in neo4j database

---
+++ CrudNeo4J-MongoDB/src/main/java/com/pauldirac/CrudNeo4J/MongoDB/Model/DoctorModelNeo.java
@@ -0,0 +1,32 @@
+package com.pauldirac.CrudNeo4J.MongoDB.Model;
+
+import java.util.Set;
+
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Relationship;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Node
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DoctorModelNeo {
+
+    @Id
+    private Long cedula;
+
+    private String nombre;
+    private String apellido;
+    private Long telefono;
+    private String correo;
+    private String estado;
+    private String especialidad;
+
+    @Relationship(type = "Atiende", direction = Relationship.Direction.INCOMING)
+    private Set<CitaModelNeo> citasAtendidas;
+
+}
