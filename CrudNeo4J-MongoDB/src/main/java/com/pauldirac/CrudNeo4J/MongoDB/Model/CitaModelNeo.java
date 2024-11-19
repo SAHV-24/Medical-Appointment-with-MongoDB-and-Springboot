3ab407b32d10e5b030ddb253a86bc7886ae39392
Jean Paul Delgado <jean.delgado@uao.edu.co>
2024-11-19 18:21:10 -0500

    added 'CitaModelNeo' for appointments stored in neo4j database

---
+++ CrudNeo4J-MongoDB/src/main/java/com/pauldirac/CrudNeo4J/MongoDB/Model/CitaModelNeo.java
@@ -0,0 +1,39 @@
+package com.pauldirac.CrudNeo4J.MongoDB.Model;
+
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Relationship;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Node
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CitaModelNeo {
+
+    @Id
+    private Long citaId;
+
+    private Long usuarioId;
+    private Long medicoId;
+
+    private String prioridad;
+    private Date fechaYHora;
+    private String consultorio;
+    private String estado;
+
+    @Relationship(type = "Tiene", direction = Relationship.Direction.INCOMING)
+    private UsuarioModelNeo usuario;
+
+    @Relationship(type = "Atiende", direction = Relationship.Direction.OUTGOING)
+    private UsuarioModelNeo doctor;
+
+}
+
+
+
