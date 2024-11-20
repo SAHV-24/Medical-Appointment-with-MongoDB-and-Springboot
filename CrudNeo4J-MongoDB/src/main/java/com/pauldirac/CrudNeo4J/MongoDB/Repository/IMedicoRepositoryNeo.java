977a6d8b100bb121d9482428297c4178bb18ebd9
Jean Paul Delgado <jean.delgado@uao.edu.co>
2024-11-19 19:49:24 -0500

    Added MedicoRepository for querying

--- CrudNeo4J-MongoDB/src/main/java/com/pauldirac/CrudNeo4J/MongoDB/Repository/IMedicoRepositoryNeo.java
+++ CrudNeo4J-MongoDB/src/main/java/com/pauldirac/CrudNeo4J/MongoDB/Repository/IMedicoRepositoryNeo.java
@@ -3,5 +3,5 @@ package com.pauldirac.CrudNeo4J.MongoDB.Repository;
 import com.pauldirac.CrudNeo4J.MongoDB.Model.DoctorModelNeo;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 
-public interface IMedicoRepositoryNeo extends Neo4jRepository<DoctorModelNeo, String> {
+public interface IMedicoRepositoryNeo extends Neo4jRepository<DoctorModelNeo, Long> {
 }
