c1b5d5d345bac2ebdf5d54678d08c98901dffe3f
Jean Paul Delgado <jean.delgado@uao.edu.co>
2024-11-19 19:50:03 -0500

    Added CitaRepository for querying

--- CrudNeo4J-MongoDB/src/main/java/com/pauldirac/CrudNeo4J/MongoDB/Repository/ICitaRepositoryNeo.java
+++ CrudNeo4J-MongoDB/src/main/java/com/pauldirac/CrudNeo4J/MongoDB/Repository/ICitaRepositoryNeo.java
@@ -3,5 +3,5 @@ package com.pauldirac.CrudNeo4J.MongoDB.Repository;
 import com.pauldirac.CrudNeo4J.MongoDB.Model.CitaModelNeo;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 
-public interface ICitaRepositoryNeo extends Neo4jRepository<CitaModelNeo, String> {
+public interface ICitaRepositoryNeo extends Neo4jRepository<CitaModelNeo, Long> {
 }
