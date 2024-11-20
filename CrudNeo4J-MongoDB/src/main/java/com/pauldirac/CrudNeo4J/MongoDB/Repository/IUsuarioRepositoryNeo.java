502720842dffd625a7a0ee5d35db317597aae6a9
Jean Paul Delgado <jean.delgado@uao.edu.co>
2024-11-19 19:18:53 -0500

    Added UsuarioRepository for querying

---
+++ CrudNeo4J-MongoDB/src/main/java/com/pauldirac/CrudNeo4J/MongoDB/Repository/IUsuarioRepositoryNeo.java
@@ -0,0 +1,21 @@
+package com.pauldirac.CrudNeo4J.MongoDB.Repository;
+
+import com.pauldirac.CrudNeo4J.MongoDB.Model.UsuarioModelNeo;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IUsuarioRepositoryNeo extends Neo4jRepository<UsuarioModelNeo, String> {
+
+    Optional<List<UsuarioModelNeo>> findUsuarioModelNeoByEdadBetweenAndEps(Integer min, Integer max, String eps);
+
+    @Query("MATCH (u:Usuario)-[:Tiene]->(c:Cita) " +
+            "WHERE u.edad > 65 AND any(condicion IN u.condicionesEspeciales WHERE toLower(condicion) IN ['hipertensión', 'diabetes', 'cáncer']) " +
+            "SET c.prioridad = 'Alta'")
+    void cambiarPrioridadCita();
+
+    Optional<List<UsuarioModelNeo>> findByEdadGreaterThanAndEps(Integer edad, String eps);
+
+}
